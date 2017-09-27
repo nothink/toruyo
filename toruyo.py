6a83dea7b44f65aea72ea1de7fc121810e144f85
nothink <nothing@yasagure.jp>
2017-09-27 18:10:42 +0900

    Fix packages.

--- toruyo.py
+++ toruyo.py
@@ -4,6 +4,7 @@
 from toruyo import proxy
 
 import sys
+from multiprocessing import Process
 
 if __name__ == "__main__":
     port = 37893
@@ -11,5 +12,7 @@ if __name__ == "__main__":
         port = int(sys.argv[1])
 
     print("Starting cache proxy on port %d" % port)
-    proxy.run_proxy(port)
+#    proxy.run_proxy(port)
+    proxy_process = Process(target=proxy.run_proxy, args=[port])
+    proxy_process.start()
     print("Started.")
